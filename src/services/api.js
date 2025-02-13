76da8e90a738013e4d8310679fdd75b2b5e2cce7
Khalid Auwal <khalidauwal732@gmail.com>
2025-02-13 12:01:58 +0100

    Added project files

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "263a9e0fcb2a6b0e651d50d23d7b0937";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
